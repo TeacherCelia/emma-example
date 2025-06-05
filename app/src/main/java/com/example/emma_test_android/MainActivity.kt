b0dbe50c3d98d8e93997c01ca37b44456b722270
Celia Pérez Vargas <celia.perez@ARKANA-PODA-GUSTAVO-R.local>
2025-06-05 14:12:41 +0200

    first view of the app

--- app/src/main/java/com/example/emma_test_android/MainActivity.kt
+++ app/src/main/java/com/example/emma_test_android/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.example.emma_test_android
 
-class MainActivity {
-    
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
